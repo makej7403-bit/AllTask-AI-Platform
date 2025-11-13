96d011c765678edc7adf94cba2ceea9e6e74fc9b
makej7403-bit <makej7403@gmail.com>
2025-11-13 22:37:05 +0000

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Welcome to AllTask AI Platform</h1>
+      <p>Your AI system created by Akin S. Sokpah from Nimba County, Liberia.</p>
+    </main>
+  );
+}
