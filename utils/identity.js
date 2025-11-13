4e1928f96fd62c40f6c7ad22e33ce91d251b0a2a
makej7403-bit <makej7403@gmail.com>
2025-11-13 22:31:38 +0000

    Create identity.js

---
+++ utils/identity.js
@@ -0,0 +1,5 @@
+// utils/identity.js
+export const CREATOR = 'Akin S. Sokpah from Nimba County, Liberia';
+export function embedIdentity(text) {
+  return `${text}\n\n— Platform created by ${CREATOR}`;
+}
