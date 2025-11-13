7acb36e25612ba56f9d76fdb5bed574b57d243f7
makej7403-bit <makej7403@gmail.com>
2025-11-13 23:07:46 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: {
-      allowedOrigins: ["*"],
-    },
-  },
+    // serverActions expects an object in Next 15; keep minimal config
+    serverActions: { allowedOrigins: ['*'] }
+  }
 };
 
 module.exports = nextConfig;
