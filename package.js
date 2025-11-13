d6f52fe238ff9b8977acbdf3d64215b950ac4ce8
makej7403-bit <makej7403@gmail.com>
2025-11-13 21:08:10 +0000

    Create package.js

---
+++ package.js
@@ -0,0 +1,16 @@
+{
+  "name": "fulltask-global-ai",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "generate": "node generate_modules.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "body-parser": "^1.20.2",
+    "cors": "^2.8.5",
+    "fs-extra": "^11.1.1",
+    "dotenv": "^16.4.0"
+  }
+}
