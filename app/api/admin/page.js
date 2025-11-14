dad23d26dbe235832f098b53947d1c90fe8057de
makej7403-bit <makej7403@gmail.com>
2025-11-14 00:56:09 +0000

    Create page.js

---
+++ app/api/admin/page.js
@@ -0,0 +1,8 @@
+export default function Admin() {
+  return (
+    <div>
+      <h1 className="text-3xl">Admin Dashboard</h1>
+      <p>Manage Users, Subscriptions, Features, Logs</p>
+    </div>
+  );
+}
